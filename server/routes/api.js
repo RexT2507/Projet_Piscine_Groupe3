f0bd78d4bedec3d841458cb849bd320ee77edd3e
RexT2507 <baptiste.marmierskyrim@hotmail.fr>
2019-10-03 21:56:23 +0200

    Connexion à la MongoDB

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+const mongoose = require('mongoose');
+const url = 'mongodb://127.0.0.1:27017';
+const dbName = 'projetsdb';
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+}
+
+mongoose.connect(`${url}/${dbName}`, options);
+
+const database = mongoose.connection;
+
+database.once('open', _ => {
+    console.log(`Connexion MongoDB: ${url}`);
+    console.log(`Database: ${dbName}`);
+});
+
+database.on('error', err => {
+    console.error('Erreur de connexion:', err);
+});
+
 router.get('/', (req, res) => {
     res.send(`
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
