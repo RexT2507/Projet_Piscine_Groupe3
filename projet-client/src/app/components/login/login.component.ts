433696a45796c53179eea6276cb0d8ddaf217023
RexT2507 <baptiste.marmierskyrim@hotmail.fr>
2019-10-09 10:17:02 +0200

    [DEV] Création de l'interface utilisateur login

--- projet-client/src/app/components/login/login.component.ts
+++ projet-client/src/app/components/login/login.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginUserData = {};
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  loginUser() {
+    console.log(this.loginUserData);
   }
 
 }
