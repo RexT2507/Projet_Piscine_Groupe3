ad9cad0263d1d85108e3400c3bcaca7020ce6fe0
Eschyle <francois.caillat@viacesi.fr>
2019-10-10 09:11:08 +0200

    revert previous commit

--- projet-client/src/app/app.component.ts
+++ projet-client/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-routing',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  title = 'projet-client';
 }
